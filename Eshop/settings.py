ea92bbc25961ffa7ea0c4ca87b749ba98ba9f672
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2021-11-26 00:08:04 +0530

    change databases

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -79,9 +79,28 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'new': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'ashu2468',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'blog',
+#         'USER': 'root',
+#         'PASSWORD': 'ashu2468',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
