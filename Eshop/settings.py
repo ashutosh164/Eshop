e7b8b1478250bfe30457043ffbc0896b2d854a88
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2024-01-11 21:01:05 +0530

    change database

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -80,14 +80,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'new': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'USER': 'root',
-        'PASSWORD': 'ashu2468',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
+    # 'new': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'blog',
+    #     'USER': 'root',
+    #     'PASSWORD': 'ashu2468',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    # }
 }
 
 
